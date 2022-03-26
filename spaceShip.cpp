27ec7c50fe46f764e28c6df2cad47c713967b5e3
Vladimir Rogozin <vladimirrogozin21@gmail.com>
2022-03-26 19:28:29 +0300

    Добавил передвижение ракеты

--- spaceShip.cpp
+++ spaceShip.cpp
@@ -5,12 +5,13 @@
 
 spaceShip::spaceShip() {
     spaceShip_x = spaceShip_y = 0.5f;
-    spaceShipSpeed_x = 5.f / screen.getParametrizationScreen().first;
-    spaceShipSpeed_y = 5.f / screen.getParametrizationScreen().second;
-    spaceShipWidth = 128;
-    spaceShipHeight = 128;
+    spaceShipSpeed_x = 0.f / screen.getParametrizationScreen().first;
+    spaceShipSpeed_y = 0.f / screen.getParametrizationScreen().second;
+    spaceShipWidth = 128.f;
+    spaceShipHeight = 128.f;
+    float spaceShipMasse = 1;
     
-    if (!textureShip.loadFromFile("image/spaceShip.png")) {
+    if (!textureShip.loadFromFile("spaceShip.png")) {
         exit(EXIT_FAILURE);
     }
 }
@@ -23,20 +24,50 @@ void spaceShip::drawSprite(sf::RenderWindow& gameWindow) {
     gameWindow.draw(spaceShipSprite);
 }
 
-void spaceShip::moveShip(float dt, char move) {
-    switch (move)
-    {
-        case 'l': spaceShip_x -= dt * spaceShipSpeed_x; break;
-        case 'r': spaceShip_x += dt * spaceShipSpeed_x; break;
-        case 'u': spaceShip_y -= dt * spaceShipSpeed_y; break;
-        case 'd': spaceShip_y += dt * spaceShipSpeed_y; break;
-    }
-}
-pair<float, float> spaceShip::getCoordinates() {
+pair<float, float> spaceShip::getCoordinates() const {
     float x = spaceShip_x;
     float y = spaceShip_y;
     pair<float, float> result = make_pair(x, y);
     return result;
 }
 
-spaceShip::~spaceShip() {}
+float spaceShip::getMasse() const {
+    return spaceShipMasse;
+}
+
+//Изменяем скорости корабля
+pair<float, float> spaceShip::increaseSpeed_x(float additionalSpeed_x) {
+    pair<float, float> v0_x_v_x = make_pair(spaceShipSpeed_x, spaceShipSpeed_x + additionalSpeed_x  / screen.getParametrizationScreen().first);
+    spaceShipSpeed_x += additionalSpeed_x  / screen.getParametrizationScreen().first;
+}
+pair<float, float> spaceShip::increaseSpeed_y(float additionalSpeed_y) {
+    pair<float, float> v0_y_v_y = make_pair(spaceShipSpeed_y, spaceShipSpeed_y + additionalSpeed_y  / screen.getParametrizationScreen().second);
+    spaceShipSpeed_y += additionalSpeed_y  / screen.getParametrizationScreen().second;
+
+}
+//Проверяем есть ли скорость(ненулевая)
+bool spaceShip::isSpeed_x() const {
+    return spaceShipSpeed_x != 0;
+}
+bool spaceShip::isSpeed_y() const {
+    return spaceShipSpeed_y != 0;
+}
+
+sf::Vector2f spaceShip::getSpaceShipSpeed() const {
+    sf::Vector2f result {spaceShipSpeed_x, spaceShipSpeed_y};
+    return result;
+}
+
+void spaceShip::moveShip(float dt) {
+    if (isSpeed_x()) {
+        spaceShip_x += spaceShipSpeed_x * dt;
+    }
+    if (isSpeed_y()) {
+        spaceShip_y += spaceShipSpeed_y * dt;
+    }
+}
+//Остановка корабля
+void spaceShip::tormoz() {
+    spaceShipSpeed_x = 0;
+    spaceShipSpeed_y = 0;
+}
