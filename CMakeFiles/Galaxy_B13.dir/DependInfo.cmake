263980cb531fe2bee263edb7dd757c02a4aa403c
Azamat Usmanov <usmanov.ak@phystech.edu>
2022-03-29 19:25:49 +0300

    Add file for CMake

---
+++ CMakeFiles/Galaxy_B13.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/azamath/Repositories/Galaxy_B13/Planet.cpp" "CMakeFiles/Galaxy_B13.dir/Planet.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/Planet.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/camera.cpp" "CMakeFiles/Galaxy_B13.dir/camera.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/camera.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/drawAll.cpp" "CMakeFiles/Galaxy_B13.dir/drawAll.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/drawAll.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/functions.cpp" "CMakeFiles/Galaxy_B13.dir/functions.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/functions.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/main.cpp" "CMakeFiles/Galaxy_B13.dir/main.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/main.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/parametrizationScreen.cpp" "CMakeFiles/Galaxy_B13.dir/parametrizationScreen.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/parametrizationScreen.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/spaceObjects.cpp" "CMakeFiles/Galaxy_B13.dir/spaceObjects.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/spaceObjects.cpp.o.d"
+  "/Users/azamath/Repositories/Galaxy_B13/spaceShip.cpp" "CMakeFiles/Galaxy_B13.dir/spaceShip.cpp.o" "gcc" "CMakeFiles/Galaxy_B13.dir/spaceShip.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
